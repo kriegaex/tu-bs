df8f6ca40b7e9899125bb5b2b58d8948c73aab38
mas <m.scheer@tu-bs.de>
2013-12-13 12:42:11 +0100

    Implementation of VowelRotator methods encode and decode

--- src/main/java/de/scrum_master/tu_bs/HelloWorld.java
+++ src/main/java/de/scrum_master/tu_bs/HelloWorld.java
@@ -1,17 +1,21 @@
-package de.scrum_master.tu_bs;
+    package de.scrum_master.tu_bs;
 
-public class HelloWorld {
-	String createGreeting(String name) {
-		return "Hello " + name + "!";
-	}
+    public class HelloWorld {
+        String createGreeting(String name) {
+            return "Hello " + name + "!";
+        }
 
-    String sayGoodbye(String name) {
-        return "Goodbye " + name + "!";
+
+        String sayGoodbye(String name) {
+            return "Goodbye " + name + "!";
+        }
+
+        public static void main(String[] args) {
+            HelloWorld hello = new HelloWorld();
+            System.out.println(hello.createGreeting("Master"));
+            System.out.println(hello.sayGoodbye("Master"));
+
+
+
+        }
     }
-
-	public static void main(String[] args) {
-		HelloWorld hello = new HelloWorld();
-		System.out.println(hello.createGreeting("Master"));
-        System.out.println(hello.sayGoodbye("Master"));
-	}
-}
--- src/main/java/de/scrum_master/tu_bs/VowelRotator.java
+++ src/main/java/de/scrum_master/tu_bs/VowelRotator.java
@@ -2,10 +2,54 @@ package de.scrum_master.tu_bs;
 
 public class VowelRotator {
 	public String encode(String originalText) {
-		return "";
+		return rotate(-1, originalText);
 	}
 
 	public String decode(String encodedText) {
-		return "";
+		return rotate(+1, encodedText);
 	}
+
+
+
+    char rotateChar(int n, char c) {
+        char [] vowelsLow= {'a','e','i','o','u'};
+
+        boolean upperCase=Character.isUpperCase(c);
+
+        c = Character.toLowerCase(c);
+
+        int pos = 0;
+
+        while ( pos < 5 && vowelsLow[pos] != c) pos++;
+
+        if (pos < 5) {
+
+            pos += (5 + n);
+            pos %= 5;
+
+            c = vowelsLow[pos];
+        }
+
+        if (upperCase) c = Character.toUpperCase(c);
+
+        return c;
+    }
+
+
+    String rotate(int n, String s) {
+
+        String r="";
+
+
+        for (char c:s.toCharArray()){
+
+            r += rotateChar(n, c);
+
+        }
+
+        return r;
+    }
+
+
+
 }
