4082002247916fa746d044f49a8beb59b1770098
Alexander Kriegisch <Alexander@Kriegisch.name>
2013-12-13 12:18:31 +0100

    Add test for HelloWorld.sayGoodbye()

--- src/test/java/de/scrum_master/tu_bs/HelloWorldTest.java
+++ src/test/java/de/scrum_master/tu_bs/HelloWorldTest.java
@@ -4,10 +4,19 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HelloWorldTest {
+	private final HelloWorld helloWorld = new HelloWorld();
+
 	@Test
 	public void testCreateGreeting() throws Exception {
-		assertEquals("Hello world!", new HelloWorld().createGreeting("world"));
-		assertEquals("Hello Alexander!", new HelloWorld().createGreeting("Alexander"));
-		assertEquals("Hello Braunschweig!", new HelloWorld().createGreeting("Braunschweig"));
+		assertEquals("Hello world!", helloWorld.createGreeting("world"));
+		assertEquals("Hello Alexander!", helloWorld.createGreeting("Alexander"));
+		assertEquals("Hello Braunschweig!", helloWorld.createGreeting("Braunschweig"));
+	}
+	
+	@Test
+	public void testSayGoodbye() throws Exception {
+		assertEquals("Goodbye world!", helloWorld.sayGoodbye("world"));
+		assertEquals("Goodbye Alexander!", helloWorld.sayGoodbye("Alexander"));
+		assertEquals("Goodbye Braunschweig!", helloWorld.sayGoodbye("Braunschweig"));
 	}
 }
